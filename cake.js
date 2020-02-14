2c2ed2b7b08f2e6ea7b56f023175190fd8eb31df
Catherine <catherineep4189@gmail.com>
2020-02-14 20:49:46 +0000

    display array values via html element by id dom method

--- cake.js
+++ cake.js
@@ -1,1 +1,9 @@
-let availableCakes = ['Rainbow', 'Chocolate', 'Florentina', 'Victoria Sponge', 'Red Velvet'];
+// ARRAY LIST
+let availableCakes = ['Rainbow', ' Chocolate', ' Florentina', ' Victoria Sponge', ' Red Velvet'];
+
+
+// TARGET ID HTML ELEMENT TO DISPLAY ARRAY VALUES
+let showAvailable = document.getElementById('takeYourPick');
+
+// CAN BE CHANGED USING BRACKET NOTATION TO SHOW VALUE AT A SPECIFIC INDEX
+showAvailable.textContent = availableCakes;
