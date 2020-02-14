5ebe770b2fc375a9915e9d68e47c1d230660ec1f
Catherine <catherineep4189@gmail.com>
2020-02-14 21:41:33 +0000

    index value

--- cake.js
+++ cake.js
@@ -2,8 +2,17 @@
 let availableCakes = ['Rainbow', ' Chocolate', ' Florentina', ' Victoria Sponge', ' Red Velvet'];
 
 
-// TARGET ID HTML ELEMENT TO DISPLAY ARRAY VALUES
+// TARGET ID HTML ATTRIBUTE TO DISPLAY ARRAY VALUES - DOM METHOD 
 let showAvailable = document.getElementById('takeYourPick');
 
 // CAN BE CHANGED USING BRACKET NOTATION TO SHOW VALUE AT A SPECIFIC INDEX
+// TO SHOW ALL VALUES, DON'T USE BRACKET NOTATION
 showAvailable.textContent = availableCakes;
+
+let chosenCake = document.getElementById('inTheOven');
+
+chosenCake.textContent = availableCakes[0];
+
+
+
+
