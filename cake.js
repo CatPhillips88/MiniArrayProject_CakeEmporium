13f18a42a6e82fc7b6a789fe0d6dae2afbde672a
Catherine <catherineep4189@gmail.com>
2020-02-15 17:04:39 +0000

    update array value via bracket notation index position

--- cake.js
+++ cake.js
@@ -1,17 +1,29 @@
-// ARRAY LIST
+// ARRAY LIST OF CAKES 
 let availableCakes = ['Rainbow', ' Chocolate', ' Florentina', ' Victoria Sponge', ' Red Velvet'];
 
 
 // TARGET ID HTML ATTRIBUTE TO DISPLAY ARRAY VALUES - DOM METHOD 
 let showAvailable = document.getElementById('takeYourPick');
 
-// CAN BE CHANGED USING BRACKET NOTATION TO SHOW VALUE AT A SPECIFIC INDEX
-// TO SHOW ALL VALUES, DON'T USE BRACKET NOTATION
+
+// BRACKET NOTATION WITH INDEX POSITION TO CHANGE ARRAY VALUE OF CAKE
+
+availableCakes[1] = ' Millie-Feuille';
+
+// textContent RETURNS STRING TEXT CONTENT FROM THE ARRAY 
+// TO SHOW ALL ARRAY VALUES, DON'T USE BRACKET NOTATION
 showAvailable.textContent = availableCakes;
 
 let chosenCake = document.getElementById('inTheOven');
 
+// CAN BE CHANGED USING BRACKET NOTATION TO SHOW VALUE AT A SPECIFIC INDEX
 chosenCake.textContent = availableCakes[0];
+
+
+
+
+
+
 
 
 
